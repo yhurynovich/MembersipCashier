92c89f57b12a09707e2e5d998ae57c8b2114e8eb
valpetruk <valpetruk@gmail.com>
2017-09-05 23:47:55 -0400

    Fixed issue with modal dialog

--- MembershipCashierW/ScriptControls/Controllers/AddProductCtrl.js
+++ MembershipCashierW/ScriptControls/Controllers/AddProductCtrl.js
@@ -1,10 +1,10 @@
 ﻿'use strict'
 
 htm.app.controller('AddProductCtrl',
-    ['$scope', '$timeout', '$http', '$uibModalInstance',
+    ['$scope', '$timeout', '$http', '$uibModalInstance', 'user',
     function ($scope, $timeout, $http, $uibModalInstance, user) {
         //var ctrl = this;
-        $scope.selectedUser = user;
+        //$scope.selectedUser = user;
         $scope.products = [];
         $scope.loading = false;
         //ctrl.filter = $routeParams.filter;
@@ -18,9 +18,19 @@ htm.app.controller('AddProductCtrl',
             }
             $http({
                 method: 'GET',
-                url: appRoot + 'api/Product?request.productLambda=x%253D%253Ex.Description%253D%253D' + ctrl.searchString
+                url: appRoot + 'api/Product?request.productLambda=x%253D%253Ex.Description%253D%253D%22' + $scope.searchString + '%22'
             }).then(function successCallback(response) {
                 $scope.products = response.data;
+                $scope.products.push({
+                    Description: "Spa"
+                });
+                $scope.products.push({
+                    Description: "Soup"
+                });
+                $scope.products.push({
+                    Description: "Test"
+                });
+
                 $scope.loading = false;
             }, function errorCallback(response) {
                 $scope.loading = false;                
@@ -28,7 +38,7 @@ htm.app.controller('AddProductCtrl',
         };
 
         $scope.cancel = function () {
-            $scope.$uibModalInstance.close();
+            $uibModalInstance.dismiss('cancel');
         };
 
         $scope.loadProducts = function () {
